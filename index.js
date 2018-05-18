13fc3d49d224bbf19cd8a6d89b26d771451153cf
Rameshwar Samindre <rameshwar.samindre@xoriant.com>
2018-05-18 17:31:10 +0530

    index html serve

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var http = require('http');
+var fs = require('fs');
+
 
 http.createServer(function(req,res){
-	res.writeHead(200,{"Content-Type": "text/plain"})
-	res.end("Hello World\n")
+	fs.readFile("index.html", function(err, data){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(data);
+  response.end();
+});
 }).listen(process.env.PORT)
