0a7ce6ef3998a5175f0c0ead38cb3d37965ff1f2
Rameshwar Samindre <rameshwar.samindre@xoriant.com>
2018-05-21 12:38:12 +0530

    Profile photo & details updated

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ let app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
+const port = 8181;
 app.use(express.static(path.join(__dirname,'/public')));
 
 
 app.get('/',function(req,res){
      res.sendFile('index.htm',{root:path.join(__dirname,'/')});
 });
-app.listen(process.env.PORT,function(){
-	console.log('listening ')
+app.listen(process.env.PORT || port,function(){
+	console.log('listening '+port)
 });
